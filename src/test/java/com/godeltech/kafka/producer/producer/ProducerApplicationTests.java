56cae2c0e7d16f1e56468b1fa5784be1f7805af7
BorisDashko <Barys.Dashko@bglgroup.co.uk>
2021-06-22 10:26:11 +0300

    Empty project created

---
+++ src/test/java/com/godeltech/kafka/producer/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.godeltech.kafka.producer.producer;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ProducerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
